d5624c9e612531b66aab165ca367d3eaf1cdcfc1
ibnuhamzah404 <47588525+ibnuhamzah404@users.noreply.github.com>
2023-04-27 18:28:10 +0700

    Add files via upload

---
+++ mforum-revisi-new/order/order2.js
@@ -0,0 +1,64 @@
+//server 1
+$(document).ready(function(){
+
+	$("#form2").on("submit", function(event){
+
+	  event.preventDefault(); // menghentikan sejenak proses submit form
+
+	  let name  = $("#name2").val(); // mengambil data dari input name
+	  let hp = $("#phone2").val(); // mengambil data dari input phone
+
+	  console.log(name, hp);
+
+	  let fields = { // field yang dikirimkan ke server
+	    name,
+	    hp,
+	    page
+	  };
+
+	  let token = 'keyTrpHYG7ZXBUxRR'; // token untuk autentikasi api
+
+	  axios.post('https://api.airtable.com/v0/appIThdR8xqGUVXTa/Table%201', { // proses pengiriman data ke server menggunakan axios
+	      fields
+	  },{
+	      headers: {
+	          'Authorization': 'Bearer ' + token ,
+	          'Content-Type': 'application/json'
+	      },
+	  })
+	  .then(data => {
+	    console.log('testing');
+	    $(this).unbind('submit').submit(); // continue the submit unbind preventDefault
+	  })
+
+	});
+
+var forms = document.getElementsByTagName('form2');
+	for (var i = 0; i < forms.length; i++) {
+		forms[i].setAttribute('action', 'order/thanks-indo2.html');
+		forms[i].setAttribute('method', 'get');
+};
+
+var tmp = new Array();
+var tmp2 = new Array();
+var param = new Array();
+
+var get = location.search;
+if(get != '') {
+	tmp = (get.substr(1)).split('&');
+	for(var i=0; i < tmp.length; i++) {
+		tmp2 = tmp[i].split('=');
+		param[tmp2[0]] = tmp2[1];
+		}
+	}
+jQuery.each( $('form2[action="order/thanks-indo2.html"]'), function( i, val ) {
+		for (var key in param) {
+			if ($(val).find("input[name=" + key + "]").length == 0) {
+			$(val).append("<input type=\"hidden\" name=\"" + key + "\" value=\"" + param[key]+ "\" />");
+			}
+			else {var oldInput = $(val).find('input[name=' + key + ']');
+				oldInput[0].value = param[key];
+			};
+		};
+	});
+});
